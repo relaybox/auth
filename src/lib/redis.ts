7bc0675e612c997aef4bf279673df660fd17b0f8
livecloud-labs <livecloud.store@gmail.com>
2024-10-17 13:54:08 +0100

    chore: update config to include env vars

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,12 +1,7 @@
-import { getLogger } from '@/util/logger.util';
-
-const logger = getLogger('redis');
-
 const REDIS_HOST = process.env.REDIS_HOST;
 const REDIS_PORT = process.env.REDIS_PORT;
 const REDIS_AUTH_TOKEN = process.env.REDIS_AUTH_TOKEN;
 const REDIS_TLS_DISABLED = process.env.REDIS_TLS_DISABLED === 'true';
-// const REDIS_AUTH_TOKEN = getRedisAuthToken();
 
 const tlsConnectionOptions = {
   tls: true,
@@ -17,15 +12,6 @@ const tlsConnectionOptionsIo = {
   password: REDIS_AUTH_TOKEN,
   tls: tlsConnectionOptions
 };
-
-// function getRedisAuthToken(): string {
-//   if (!REDIS_AUTH) {
-//     logger.warn('Redis auth token for TLS connection not defined');
-//     return '';
-//   }
-
-//   return JSON.parse(REDIS_AUTH).authToken;
-// }
 
 export const connectionOptionsIo = {
   host: REDIS_HOST!,
