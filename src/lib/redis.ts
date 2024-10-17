df5adc4dcbf6a2c47ea6462339b44a800e1892e3
livecloud-labs <livecloud.store@gmail.com>
2024-10-17 11:48:15 +0100

    chore: update redis env keys

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -4,9 +4,9 @@ const logger = getLogger('redis');
 
 const REDIS_HOST = process.env.REDIS_HOST;
 const REDIS_PORT = process.env.REDIS_PORT;
-const REDIS_AUTH = process.env.REDIS_AUTH;
+const REDIS_AUTH_TOKEN = process.env.REDIS_AUTH_TOKEN;
 const REDIS_TLS_DISABLED = process.env.REDIS_TLS_DISABLED === 'true';
-const REDIS_AUTH_TOKEN = getRedisAuthToken();
+// const REDIS_AUTH_TOKEN = getRedisAuthToken();
 
 const tlsConnectionOptions = {
   tls: true,
@@ -18,14 +18,14 @@ const tlsConnectionOptionsIo = {
   tls: tlsConnectionOptions
 };
 
-function getRedisAuthToken(): string {
-  if (!REDIS_AUTH) {
-    logger.warn('Redis auth token for TLS connection not defined');
-    return '';
-  }
+// function getRedisAuthToken(): string {
+//   if (!REDIS_AUTH) {
+//     logger.warn('Redis auth token for TLS connection not defined');
+//     return '';
+//   }
 
-  return JSON.parse(REDIS_AUTH).authToken;
-}
+//   return JSON.parse(REDIS_AUTH).authToken;
+// }
 
 export const connectionOptionsIo = {
   host: REDIS_HOST!,
